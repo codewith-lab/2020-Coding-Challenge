6da372eecd2899cba773b4dedcb2f80f9a135b85
codewith-lab <gq2142@columbia.edu>
2025-04-14 00:09:24 -0400

    Add files via upload

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,9 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      //update the scoreboard
+      let scoreboard = result.scoreboard;
+      display_scoreboard(scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
